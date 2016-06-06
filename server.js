e338dafb71cc5426da69e60c1ee0b7ca77b145e3
Patrick Ferreira <paatrickferreira@gmail.com>
2016-06-06 11:39:11 -0300

    Added socket.io library

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const express = require('express');
 const app     = express();
 const PORT    = process.env.PORT || 3000;
 const http    = require('http').Server(app);
+const io      = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function() {
+	console.log('User connected via socket.io!');
+});
   
 http.listen(PORT, function() {
 	console.log('Server started on port:' + PORT);
