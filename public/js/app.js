50567e21d7898b42ba72e0cd9b776fc8fa2c87ef
Patrick Ferreira <paatrickferreira@gmail.com>
2016-06-20 16:11:19 -0300

    Basic broadcast structure

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,22 @@
 
 var socket = io();
 
+/**
+ * Used to handle connection
+ * 
+ * @return void
+ */
 socket.on('connect', function () {
 	console.log('Connected to Socket.io!');
+});
+
+/**
+ * Used to handle a new user's message
+ * 
+ * @param Message object
+ * @return void
+ */
+socket.on('message', function (message) {
+	console.log('New message: ');
+	console.log(message.text);
 });
